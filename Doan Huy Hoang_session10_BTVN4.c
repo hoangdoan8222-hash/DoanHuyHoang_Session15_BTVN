6c1780755dc30260d89e85f74f6e68e0fa35f949
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-11-12 23:17:26 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session10_BTVN4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, temp;
+    printf("nhap so phan tu: ");
+    scanf("%d", &n);
+    
+	int arr[n];
+    
+    for (i = 0; i < n; i++) {
+    	printf("Nhap cac phan tu cua mang %d: ", i);
+        scanf("%d", &arr[i]);
+    }
+   
+    for (i = 1; i < n - 1; i++) {
+        for (j = 0; j < n - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("Mang sau khi sap xep theo thu tu tang dan: ");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
+
